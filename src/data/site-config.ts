d14bc6eb3836bdbd6a2c53ef93239456b5c3dcb7
Dawntraoz <albasilventefu@gmail.com>
2024-01-27 19:17:59 +0100

    feat: social media sharing meta tags

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -1,12 +1,14 @@
 export type SiteConfig = {
   name: string;
   description: string;
+  domain: string;
 };
 
 const siteConfig: SiteConfig = {
   name: "@dawntraoz",
   description:
     "FE dev · Speaker · Podcaster · DevRel @Storyblok · Nuxt · WTM · GDE · MVP",
+  domain: "links.dawntraoz.com",
 };
 
 export default siteConfig;
